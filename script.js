5d6e15dbdc53ecfe37ffdd98cdea63a71b246302
T1T4N <163899318+The-Cod3Father@users.noreply.github.com>
2024-06-03 01:19:47 -0400

    Committ 10

--- script.js
+++ script.js
@@ -31,7 +31,13 @@ class Particle {
         this.draw();
         this.x += this.velocity.x;
         this.y += this.velocity.y;
-        this.alpha -= 0.01;
+        if (this.alpha > 0) {
+            this.alpha -= 0.005;
+        } else {
+            this.alpha = 1;
+            this.x = canvas.width / 2 + Math.random() * canvas.width / 2 * (Math.random() < 0.5 ? -1 : 1);
+            this.y = canvas.height / 2 + Math.random() * canvas.height / 2 * (Math.random() < 0.5 ? -1 : 1);
+        }
     }
 }
 
@@ -42,59 +48,25 @@ function getRandomColor() {
 
 function spawnParticles() {
     for (let i = 0; i < particleCount; i++) {
-        const angle = Math.random() * Math.PI * 2;
-        const radius = Math.sqrt(Math.random()) * canvas.width * 0.4;
-        const x = canvas.width / 2 + radius * Math.cos(angle);
-        const y = canvas.height / 2 + radius * Math.sin(angle);
+        const radius = Math.random() * 2;
         const color = getRandomColor();
         const velocity = {
             x: (Math.random() - 0.5) * 0.5,
             y: (Math.random() - 0.5) * 0.5
         };
-        particles.push(new Particle(x, y, 2, color, velocity));
-    }
-}
-
-function drawConnections() {
-    for (let i = 0; i < particles.length; i++) {
-        for (let j = i + 1; j < particles.length; j++) {
-            const dist = Math.hypot(particles[i].x - particles[j].x, particles[i].y - particles[j].y);
-            if (dist < 120) {
-                ctx.beginPath();
-                ctx.strokeStyle = `rgba(255, 255, 255, ${1 - dist / 120})`;
-                ctx.moveTo(particles[i].x, particles[i].y);
-                ctx.lineTo(particles[j].x, particles[j].y);
-                ctx.stroke();
-                ctx.closePath();
-            }
-        }
+        const x = canvas.width / 2 + Math.random() * canvas.width / 2 * (Math.random() < 0.5 ? -1 : 1);
+        const y = canvas.height / 2 + Math.random() * canvas.height / 2 * (Math.random() < 0.5 ? -1 : 1);
+        particles.push(new Particle(x, y, radius, color, velocity));
     }
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    particles.forEach((particle, index) => {
-        if (particle.alpha > 0) {
-            particle.update();
-        } else {
-            particles.splice(index, 1);
-            const angle = Math.random() * Math.PI * 2;
-            const radius = Math.sqrt(Math.random()) * canvas.width * 0.4;
-            const x = canvas.width / 2 + radius * Math.cos(angle);
-            const y = canvas.height / 2 + radius * Math.sin(angle);
-            const color = getRandomColor();
-            const velocity = {
-                x: (Math.random() - 0.5) * 0.5,
-                y: (Math.random() - 0.5) * 0.5
-            };
-            particles.push(new Particle(x, y, 2, color, velocity));
-        }
+    particles.forEach((particle) => {
+        particle.update();
     });
-
-    drawConnections();
 }
 
 spawnParticles();
